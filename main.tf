1276023f52d23763fb613334ab1be259f6e68e52
mt5225 <mt5225@gmail.com>
2021-08-27 14:00:31 -0700

    update

---
+++ main.tf
@@ -0,0 +1,13 @@
+module "obsvr" {
+  source      = "./modules/ob"        #A
+  namespace   = var.namespace         #B
+  ssh_keypair = var.ssh_keypair       #A
+  vpc         = module.networking.vpc #A
+  sg          = module.networking.sg  #A
+
+}
+
+module "networking" {
+  source    = "./modules/networking" #A
+  namespace = var.namespace          #B
+}
---
+++ modules/ob/main.tf
@@ -0,0 +1,32 @@
+data "cloudinit_config" "config" {
+  gzip          = true
+  base64_encode = true
+  part {
+    content_type = "text/cloud-config"
+    content      = file("${path.module}/cloud_config.yaml")
+  }
+}
+
+module "iam_instance_profile" {
+  source  = "../aws-base/terraform-aws-iip"
+  actions = ["logs:*", "rds:*"]
+}
+
+module "ob" {
+  source                      = "terraform-aws-modules/ec2-instance/aws"
+  version                     = "3.1.0"
+  name                        = "${var.namespace}-ec2"
+  ami                         = data.aws_ami.centos.id
+  instance_type               = "t3.large"
+  key_name                    = var.ssh_keypair
+  vpc_security_group_ids      = [var.sg.db]
+  subnet_id                   = var.vpc.public_subnets[0]
+  iam_instance_profile        = module.iam_instance_profile.name
+  associate_public_ip_address = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "demo"
+  }
+}
+
